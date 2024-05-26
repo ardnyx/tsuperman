ac38a95169060770973f0df0869f7030976f9772
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-26 21:30:59 +0800

    added the politician area form, and finished the corresponding dialogues

---
+++ InsideHouse/PA_Dialogue_Section-1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace InsideHouse
+{
+    partial class PA_Dialogue_Section_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            wall_2 = new PictureBox();
+            politician = new PictureBox();
+            protagonist = new PictureBox();
+            pictureBox4 = new PictureBox();
+            playerMsg = new Label();
+            playerName = new Label();
+            ((System.ComponentModel.ISupportInitialize)wall_2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)politician).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)protagonist).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // wall_2
+            // 
+            wall_2.BackColor = Color.Black;
+            wall_2.BorderStyle = BorderStyle.Fixed3D;
+            wall_2.Location = new Point(-13, -14);
+            wall_2.Name = "wall_2";
+            wall_2.Size = new Size(882, 233);
+            wall_2.TabIndex = 8;
+            wall_2.TabStop = false;
+            wall_2.Tag = "Wall";
+            // 
+            // politician
+            // 
+            politician.BackColor = Color.Transparent;
+            politician.Image = Properties.Resources.local_politician;
+            politician.Location = new Point(636, 255);
+            politician.Name = "politician";
+            politician.Size = new Size(137, 214);
+            politician.SizeMode = PictureBoxSizeMode.StretchImage;
+            politician.TabIndex = 15;
+            politician.TabStop = false;
+            politician.Tag = "child";
+            // 
+            // protagonist
+            // 
+            protagonist.BackColor = Color.Transparent;
+            protagonist.Image = Properties.Resources._51;
+            protagonist.Location = new Point(559, 299);
+            protagonist.Name = "protagonist";
+            protagonist.Size = new Size(100, 170);
+            protagonist.SizeMode = PictureBoxSizeMode.StretchImage;
+            protagonist.TabIndex = 17;
+            protagonist.TabStop = false;
+            protagonist.Tag = "child";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Black;
+            pictureBox4.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox4.Location = new Point(-13, 446);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(882, 78);
+            pictureBox4.TabIndex = 18;
+            pictureBox4.TabStop = false;
+            pictureBox4.Tag = "Wall";
+            // 
+            // playerMsg
+            // 
+            playerMsg.AutoSize = true;
+            playerMsg.BackColor = Color.Black;
+            playerMsg.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            playerMsg.ForeColor = Color.White;
+            playerMsg.Location = new Point(46, 102);
+            playerMsg.Name = "playerMsg";
+            playerMsg.Size = new Size(88, 28);
+            playerMsg.TabIndex = 20;
+            playerMsg.Tag = "";
+            playerMsg.Text = "Message";
+            // 
+            // playerName
+            // 
+            playerName.AutoSize = true;
+            playerName.BackColor = Color.Black;
+            playerName.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            playerName.ForeColor = Color.White;
+            playerName.Location = new Point(46, 74);
+            playerName.Name = "playerName";
+            playerName.Size = new Size(65, 28);
+            playerName.TabIndex = 19;
+            playerName.Tag = "";
+            playerName.Text = "Player";
+            // 
+            // PA_Dialogue_Section_1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.city_background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(856, 518);
+            Controls.Add(playerMsg);
+            Controls.Add(playerName);
+            Controls.Add(pictureBox4);
+            Controls.Add(protagonist);
+            Controls.Add(politician);
+            Controls.Add(wall_2);
+            DoubleBuffered = true;
+            Name = "PA_Dialogue_Section_1";
+            Text = "PA_Dialogue_Section_1";
+            Load += PA_Dialogue_Section_1_Load;
+            ((System.ComponentModel.ISupportInitialize)wall_2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)politician).EndInit();
+            ((System.ComponentModel.ISupportInitialize)protagonist).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox wall_2;
+        private PictureBox politician;
+        private PictureBox protagonist;
+        private PictureBox pictureBox4;
+        private Label playerMsg;
+        private Label playerName;
+    }
+}
---
+++ InsideHouse/PA_Dialogue_Section-1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsideHouse
+{
+    public partial class PA_Dialogue_Section_1 : Form
+    {
+        private int dialogueCounter = 0;
+        private int msgIndex = 0;
+        private string playerMsgText = "Good day Ms. Mayor, Can I speak with you for a moment?";
+        private System.Windows.Forms.Timer animationTimer;
+        public PA_Dialogue_Section_1()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(PA_Dialogue_Section_1_Load);
+            this.MouseClick += new MouseEventHandler(MouseClick_Part1);
+        }
+
+
+
+        private void protagonist_Click(object sender, EventArgs e)
+        {
+            // ACCIDENT
+        }
+
+        private void PA_Dialogue_Section_1_Load(object sender, EventArgs e)
+        {
+            playerName.Text = "Protagonist";
+            playerMsg.Text = string.Empty;
+
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+        }
+
+        private void AnimateText(object sender, EventArgs e)
+        {
+            //start of 2nd dialogue
+            if (dialogueCounter == 1)
+            {
+                playerMsgText = "Of course. What's on your mind?";
+            }
+            // 3rd dialogue
+            else if (dialogueCounter == 2)
+            {
+                playerName.Text = "Protagonist";
+                playerMsgText = "I need advice on how to fight this jeepney phaseout. It's affecting my family and so many others.";
+            }
+            // 4th dialogue
+            else if (dialogueCounter == 3)
+            {
+                playerName.Text = "Local Politician";
+                playerMsgText = "You need to raise awareness first. Start by distributing flyers, get the community involved. Show them why this matters.";
+            }
+            // 5th dialogue
+            else if (dialogueCounter == 4)
+            {
+                playerName.Text = "Protagonist";
+                playerMsgText = "Hmmm.. Aha!";
+            }
+            // 6th dialogue
+            else if (dialogueCounter == 5)
+            {
+                playerName.Text = "Protagonist";
+                playerMsgText = "How about I distribute flyers to people around here, to raise their awareness!";
+            }
+            //7th dialogue
+            else if (dialogueCounter == 6)
+            {
+                playerName.Text = "Local Politician";
+                playerMsgText = "That's a good idea!";
+            }
+            // next form
+            else if (dialogueCounter == 7)
+            {
+                // next form
+            }
+            if (msgIndex < playerMsgText.Length)
+            {
+                playerMsg.Text += playerMsgText[msgIndex];
+                msgIndex++;
+            }
+            else
+            {
+                animationTimer.Stop();
+            }
+        }
+
+        // start of 2nd dialogue
+        private void MouseClick_Part1(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerName.Text = "Local Politician";
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part1; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part2; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 3rd dialogue
+        private void MouseClick_Part2(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part2; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part3; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 4th dialogue
+        private void MouseClick_Part3(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part3; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part4; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 5th dialogue
+        private void MouseClick_Part4(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part4; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part5; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 6th dialogue
+        private void MouseClick_Part5(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part5; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part6; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 7th dialogue
+        private void MouseClick_Part6(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part6; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part7; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        //ending
+        private void MouseClick_Part7(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+        }
+    }
+}
