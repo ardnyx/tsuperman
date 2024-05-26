ae1cf10ccb12522f5fe259dcd9a4be095bafbaa4
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-26 10:40:36 +0800

    added LivingRoom_Part3 forms, and a function to switch to LeavingTheHouse form when the user reaches the leftmost edge of the form

---
+++ InsideHouse/LivingRoom_Part3.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace InsideHouse
+{
+    partial class LivingRoom_Part3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            protagonist = new PictureBox();
+            diamondPictureBox1 = new DiamondPictureBox();
+            mother = new PictureBox();
+            sibling = new PictureBox();
+            father = new PictureBox();
+            table = new RoundPictureBox();
+            Couch = new PictureBox();
+            wall_7 = new PictureBox();
+            wall_6 = new PictureBox();
+            wall_5 = new PictureBox();
+            wall_4 = new PictureBox();
+            wall_3 = new PictureBox();
+            wall_1 = new PictureBox();
+            wall_2 = new PictureBox();
+            wall_8 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)protagonist).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)diamondPictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mother).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sibling).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)father).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)table).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Couch).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_8).BeginInit();
+            SuspendLayout();
+            // 
+            // protagonist
+            // 
+            protagonist.Image = Properties.Resources._51;
+            protagonist.Location = new Point(427, 134);
+            protagonist.Name = "protagonist";
+            protagonist.Size = new Size(74, 130);
+            protagonist.SizeMode = PictureBoxSizeMode.StretchImage;
+            protagonist.TabIndex = 33;
+            protagonist.TabStop = false;
+            protagonist.Tag = "child";
+            // 
+            // diamondPictureBox1
+            // 
+            diamondPictureBox1.BackColor = Color.Khaki;
+            diamondPictureBox1.Location = new Point(545, 165);
+            diamondPictureBox1.Name = "diamondPictureBox1";
+            diamondPictureBox1.Size = new Size(57, 53);
+            diamondPictureBox1.TabIndex = 32;
+            diamondPictureBox1.TabStop = false;
+            // 
+            // mother
+            // 
+            mother.BackColor = Color.Transparent;
+            mother.Image = Properties.Resources._81;
+            mother.Location = new Point(685, 47);
+            mother.Name = "mother";
+            mother.Size = new Size(74, 128);
+            mother.SizeMode = PictureBoxSizeMode.StretchImage;
+            mother.TabIndex = 31;
+            mother.TabStop = false;
+            // 
+            // sibling
+            // 
+            sibling.BackColor = Color.Transparent;
+            sibling.Image = Properties.Resources._61;
+            sibling.Location = new Point(685, 216);
+            sibling.Name = "sibling";
+            sibling.Size = new Size(63, 118);
+            sibling.SizeMode = PictureBoxSizeMode.StretchImage;
+            sibling.TabIndex = 30;
+            sibling.TabStop = false;
+            // 
+            // father
+            // 
+            father.Image = Properties.Resources._71;
+            father.Location = new Point(523, 258);
+            father.Name = "father";
+            father.Size = new Size(95, 169);
+            father.SizeMode = PictureBoxSizeMode.StretchImage;
+            father.TabIndex = 29;
+            father.TabStop = false;
+            // 
+            // table
+            // 
+            table.BackColor = Color.Maroon;
+            table.Location = new Point(523, 131);
+            table.Name = "table";
+            table.Size = new Size(170, 163);
+            table.TabIndex = 28;
+            table.TabStop = false;
+            // 
+            // Couch
+            // 
+            Couch.BackColor = Color.Brown;
+            Couch.Location = new Point(211, 421);
+            Couch.Name = "Couch";
+            Couch.Size = new Size(217, 36);
+            Couch.TabIndex = 27;
+            Couch.TabStop = false;
+            Couch.Tag = "Couch";
+            // 
+            // wall_7
+            // 
+            wall_7.BackColor = Color.Black;
+            wall_7.Location = new Point(765, 370);
+            wall_7.Name = "wall_7";
+            wall_7.Size = new Size(95, 114);
+            wall_7.TabIndex = 26;
+            wall_7.TabStop = false;
+            wall_7.Tag = "Wall";
+            // 
+            // wall_6
+            // 
+            wall_6.BackColor = Color.Black;
+            wall_6.Location = new Point(765, 73);
+            wall_6.Name = "wall_6";
+            wall_6.Size = new Size(203, 191);
+            wall_6.TabIndex = 25;
+            wall_6.TabStop = false;
+            wall_6.Tag = "Wall";
+            // 
+            // wall_5
+            // 
+            wall_5.BackColor = Color.Black;
+            wall_5.Location = new Point(675, -3);
+            wall_5.Name = "wall_5";
+            wall_5.Size = new Size(245, 83);
+            wall_5.TabIndex = 24;
+            wall_5.TabStop = false;
+            wall_5.Tag = "Wall";
+            // 
+            // wall_4
+            // 
+            wall_4.BackColor = Color.Black;
+            wall_4.Location = new Point(405, -3);
+            wall_4.Name = "wall_4";
+            wall_4.Size = new Size(158, 83);
+            wall_4.TabIndex = 23;
+            wall_4.TabStop = false;
+            wall_4.Tag = "Wall";
+            // 
+            // wall_3
+            // 
+            wall_3.BackColor = Color.Black;
+            wall_3.Location = new Point(-2, -3);
+            wall_3.Name = "wall_3";
+            wall_3.Size = new Size(308, 83);
+            wall_3.TabIndex = 22;
+            wall_3.TabStop = false;
+            wall_3.Tag = "Wall";
+            // 
+            // wall_1
+            // 
+            wall_1.BackColor = Color.Black;
+            wall_1.Location = new Point(-2, -15);
+            wall_1.Name = "wall_1";
+            wall_1.Size = new Size(134, 279);
+            wall_1.TabIndex = 21;
+            wall_1.TabStop = false;
+            wall_1.Tag = "Wall";
+            // 
+            // wall_2
+            // 
+            wall_2.BackColor = Color.Black;
+            wall_2.Location = new Point(-2, 370);
+            wall_2.Name = "wall_2";
+            wall_2.Size = new Size(134, 87);
+            wall_2.TabIndex = 20;
+            wall_2.TabStop = false;
+            wall_2.Tag = "Wall";
+            // 
+            // wall_8
+            // 
+            wall_8.BackColor = Color.Black;
+            wall_8.Location = new Point(-2, 450);
+            wall_8.Name = "wall_8";
+            wall_8.Size = new Size(862, 75);
+            wall_8.TabIndex = 19;
+            wall_8.TabStop = false;
+            wall_8.Tag = "Wall";
+            // 
+            // LivingRoom_Part3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(856, 518);
+            Controls.Add(protagonist);
+            Controls.Add(diamondPictureBox1);
+            Controls.Add(mother);
+            Controls.Add(sibling);
+            Controls.Add(father);
+            Controls.Add(table);
+            Controls.Add(Couch);
+            Controls.Add(wall_7);
+            Controls.Add(wall_6);
+            Controls.Add(wall_5);
+            Controls.Add(wall_4);
+            Controls.Add(wall_3);
+            Controls.Add(wall_1);
+            Controls.Add(wall_2);
+            Controls.Add(wall_8);
+            Name = "LivingRoom_Part3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LivingRoom_Part3";
+            Load += LivingRoom_Part3_Load;
+            KeyDown += keyIsDown;
+            KeyUp += keyIsUp;
+            ((System.ComponentModel.ISupportInitialize)protagonist).EndInit();
+            ((System.ComponentModel.ISupportInitialize)diamondPictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mother).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sibling).EndInit();
+            ((System.ComponentModel.ISupportInitialize)father).EndInit();
+            ((System.ComponentModel.ISupportInitialize)table).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Couch).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_8).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox protagonist;
+        private DiamondPictureBox diamondPictureBox1;
+        private PictureBox mother;
+        private PictureBox sibling;
+        private PictureBox father;
+        private RoundPictureBox table;
+        private PictureBox Couch;
+        private PictureBox wall_7;
+        private PictureBox wall_6;
+        private PictureBox wall_5;
+        private PictureBox wall_4;
+        private PictureBox wall_3;
+        private PictureBox wall_1;
+        private PictureBox wall_2;
+        private PictureBox wall_8;
+    }
+}
---
+++ InsideHouse/LivingRoom_Part3.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsideHouse
+{
+    public partial class LivingRoom_Part3 : Form
+    {
+        bool goUp, goDown, goLeft, goRight;
+        int speed = 5;
+        bool dialogueFormOpen = false;
+        List<PictureBox> walls = new List<PictureBox>();
+        System.Windows.Forms.Timer moveTimer;
+        public LivingRoom_Part3()
+        {
+            InitializeComponent();
+        }
+
+        private void LivingRoom_Part3_Load(object sender, EventArgs e)
+        {
+            // Set up the Timer
+            moveTimer = new System.Windows.Forms.Timer();
+            moveTimer.Interval = 20; // Adjust as needed
+            moveTimer.Tick += new EventHandler(MoveTimer_Tick);
+            moveTimer.Start();
+
+            // Register key events
+            this.KeyDown += new KeyEventHandler(keyIsDown);
+            this.KeyUp += new KeyEventHandler(keyIsUp);
+
+            // Add your wall PictureBoxes to the list
+            // For example:
+            walls.Add(wall_1);
+            walls.Add(wall_2);
+            walls.Add(wall_3);
+            walls.Add(wall_4);
+            walls.Add(wall_5);
+            walls.Add(wall_6);
+            walls.Add(wall_7);
+            walls.Add(wall_8);
+
+            walls.Add(father);
+            walls.Add(mother);
+            walls.Add(sibling);
+            walls.Add(table);
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            MoveProtagonist();
+        }
+
+        private void MoveProtagonist()
+        {
+            if (goUp && protagonist.Top > 0 && !IsCollision(new Point(protagonist.Left, protagonist.Top - speed)))
+            {
+                protagonist.Top -= speed;
+            }
+            if (goDown && protagonist.Bottom < this.ClientSize.Height && !IsCollision(new Point(protagonist.Left, protagonist.Top + speed)))
+            {
+                protagonist.Top += speed;
+            }
+            if (goLeft && protagonist.Left > 0 && !IsCollision(new Point(protagonist.Left - speed, protagonist.Top)))
+            {
+                protagonist.Left -= speed;
+
+                // Check if the protagonist has reached the leftmost edge
+                if (protagonist.Left >= this.ClientSize.Width)
+                {
+                    ShowLeavingTheHouse();
+                }
+            }
+            if (goRight && protagonist.Right < this.ClientSize.Width && !IsCollision(new Point(protagonist.Left + speed, protagonist.Top)))
+            {
+                protagonist.Left += speed;
+            }
+        }
+
+        private void ShowLeavingTheHouse()
+        {
+            // Stop the timer to prevent further movement
+            moveTimer.Stop();
+
+            // Create and show the LeavingTheHouse form
+            
+
+            // Optionally hide or close the current form
+            this.Hide();
+        }
+        private bool IsCollision(Point newLocation)
+        {
+            // Define the lower portion of the protagonist for collision detection
+            int lowerBodyHeight = protagonist.Height / 3;
+            Rectangle lowerBodyBounds = new Rectangle(
+                newLocation.X,
+                newLocation.Y + (protagonist.Height - lowerBodyHeight),
+                protagonist.Width,
+                lowerBodyHeight
+            );
+
+            foreach (var wall in walls)
+            {
+                if (lowerBodyBounds.IntersectsWith(wall.Bounds))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
