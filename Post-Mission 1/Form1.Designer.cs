34a6c1458e429272a02b7d8a2eebd831a3cc8c97
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-27 01:45:56 +0800

    added the first dialogue for post mission 1

--- Post-Mission 1/Form1.Designer.cs
+++ Post-Mission 1/Form1.Designer.cs
@@ -28,12 +28,371 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            wall3 = new PictureBox();
+            wall5 = new PictureBox();
+            wall6 = new PictureBox();
+            wall4 = new PictureBox();
+            wall1 = new PictureBox();
+            wall2 = new PictureBox();
+            wall8 = new PictureBox();
+            wall7 = new PictureBox();
+            window = new PictureBox();
+            protagonist = new PictureBox();
+            father = new PictureBox();
+            mother = new PictureBox();
+            sibling = new PictureBox();
+            firstDialogue = new Panel();
+            Message1 = new Label();
+            Name1 = new Label();
+            firstDialogue_Companion = new Panel();
+            secondDialogue = new Panel();
+            thirdDialogue_Companion = new Panel();
+            Message2 = new Label();
+            Name2 = new Label();
+            thirdDialogue = new Panel();
+            Message3 = new Label();
+            Name3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)wall3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)window).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)protagonist).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)father).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mother).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sibling).BeginInit();
+            firstDialogue.SuspendLayout();
+            secondDialogue.SuspendLayout();
+            thirdDialogue_Companion.SuspendLayout();
+            thirdDialogue.SuspendLayout();
+            SuspendLayout();
+            // 
+            // wall3
+            // 
+            wall3.BackColor = Color.Black;
+            wall3.Location = new Point(-16, -2);
+            wall3.Name = "wall3";
+            wall3.Size = new Size(254, 81);
+            wall3.TabIndex = 0;
+            wall3.TabStop = false;
+            // 
+            // wall5
+            // 
+            wall5.BackColor = Color.Black;
+            wall5.Location = new Point(-51, 57);
+            wall5.Name = "wall5";
+            wall5.Size = new Size(174, 194);
+            wall5.TabIndex = 1;
+            wall5.TabStop = false;
+            // 
+            // wall6
+            // 
+            wall6.BackColor = Color.Black;
+            wall6.Location = new Point(-16, 369);
+            wall6.Name = "wall6";
+            wall6.Size = new Size(139, 174);
+            wall6.TabIndex = 2;
+            wall6.TabStop = false;
+            // 
+            // wall4
+            // 
+            wall4.BackColor = Color.Black;
+            wall4.Location = new Point(351, -2);
+            wall4.Name = "wall4";
+            wall4.Size = new Size(207, 81);
+            wall4.TabIndex = 3;
+            wall4.TabStop = false;
+            // 
+            // wall1
+            // 
+            wall1.BackColor = Color.Black;
+            wall1.Location = new Point(667, -2);
+            wall1.Name = "wall1";
+            wall1.Size = new Size(207, 81);
+            wall1.TabIndex = 4;
+            wall1.TabStop = false;
+            // 
+            // wall2
+            // 
+            wall2.BackColor = Color.Black;
+            wall2.Location = new Point(769, 57);
+            wall2.Name = "wall2";
+            wall2.Size = new Size(175, 212);
+            wall2.TabIndex = 5;
+            wall2.TabStop = false;
+            // 
+            // wall8
+            // 
+            wall8.BackColor = Color.Black;
+            wall8.Location = new Point(783, -18);
+            wall8.Name = "wall8";
+            wall8.Size = new Size(129, 157);
+            wall8.TabIndex = 6;
+            wall8.TabStop = false;
+            // 
+            // wall7
+            // 
+            wall7.BackColor = Color.Black;
+            wall7.Location = new Point(106, 464);
+            wall7.Name = "wall7";
+            wall7.Size = new Size(676, 128);
+            wall7.TabIndex = 7;
+            wall7.TabStop = false;
+            // 
+            // window
+            // 
+            window.BackColor = Color.Brown;
+            window.Location = new Point(206, 439);
+            window.Name = "window";
+            window.Size = new Size(207, 40);
+            window.TabIndex = 8;
+            window.TabStop = false;
+            // 
+            // protagonist
+            // 
+            protagonist.BackgroundImage = Properties.Resources.child_1;
+            protagonist.BackgroundImageLayout = ImageLayout.Stretch;
+            protagonist.Location = new Point(241, 297);
+            protagonist.Name = "protagonist";
+            protagonist.Size = new Size(73, 136);
+            protagonist.TabIndex = 9;
+            protagonist.TabStop = false;
+            // 
+            // father
+            // 
+            father.BackgroundImage = Properties.Resources.father;
+            father.BackgroundImageLayout = ImageLayout.Stretch;
+            father.Location = new Point(148, 66);
+            father.Name = "father";
+            father.Size = new Size(90, 165);
+            father.TabIndex = 10;
+            father.TabStop = false;
+            // 
+            // mother
+            // 
+            mother.BackgroundImage = Properties.Resources.mother;
+            mother.BackgroundImageLayout = ImageLayout.Stretch;
+            mother.Location = new Point(259, 85);
+            mother.Name = "mother";
+            mother.Size = new Size(86, 146);
+            mother.TabIndex = 11;
+            mother.TabStop = false;
+            // 
+            // sibling
+            // 
+            sibling.BackgroundImage = Properties.Resources.child_2;
+            sibling.BackgroundImageLayout = ImageLayout.Stretch;
+            sibling.Location = new Point(699, 57);
+            sibling.Name = "sibling";
+            sibling.Size = new Size(64, 127);
+            sibling.TabIndex = 12;
+            sibling.TabStop = false;
+            // 
+            // firstDialogue
+            // 
+            firstDialogue.BackColor = Color.Black;
+            firstDialogue.BorderStyle = BorderStyle.Fixed3D;
+            firstDialogue.Controls.Add(Message1);
+            firstDialogue.Controls.Add(Name1);
+            firstDialogue.Location = new Point(-16, -2);
+            firstDialogue.Name = "firstDialogue";
+            firstDialogue.Size = new Size(898, 173);
+            firstDialogue.TabIndex = 13;
+            // 
+            // Message1
+            // 
+            Message1.AutoSize = true;
+            Message1.BackColor = Color.Transparent;
+            Message1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            Message1.ForeColor = Color.White;
+            Message1.Location = new Point(57, 82);
+            Message1.Name = "Message1";
+            Message1.Size = new Size(88, 28);
+            Message1.TabIndex = 1;
+            Message1.Text = "Message";
+            // 
+            // Name1
+            // 
+            Name1.AutoSize = true;
+            Name1.BackColor = Color.Transparent;
+            Name1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            Name1.ForeColor = Color.White;
+            Name1.Location = new Point(57, 51);
+            Name1.Name = "Name1";
+            Name1.Size = new Size(64, 28);
+            Name1.TabIndex = 0;
+            Name1.Text = "Name";
+            // 
+            // firstDialogue_Companion
+            // 
+            firstDialogue_Companion.BackColor = Color.Black;
+            firstDialogue_Companion.BorderStyle = BorderStyle.Fixed3D;
+            firstDialogue_Companion.Location = new Point(-16, 384);
+            firstDialogue_Companion.Name = "firstDialogue_Companion";
+            firstDialogue_Companion.Size = new Size(890, 159);
+            firstDialogue_Companion.TabIndex = 14;
+            // 
+            // secondDialogue
+            // 
+            secondDialogue.BackColor = Color.Black;
+            secondDialogue.BorderStyle = BorderStyle.Fixed3D;
+            secondDialogue.Controls.Add(thirdDialogue_Companion);
+            secondDialogue.Controls.Add(Message2);
+            secondDialogue.Controls.Add(Name2);
+            secondDialogue.Location = new Point(-6, 314);
+            secondDialogue.Name = "secondDialogue";
+            secondDialogue.Size = new Size(904, 208);
+            secondDialogue.TabIndex = 14;
+            secondDialogue.Visible = false;
+            // 
+            // thirdDialogue_Companion
+            // 
+            thirdDialogue_Companion.BackColor = Color.Black;
+            thirdDialogue_Companion.BorderStyle = BorderStyle.Fixed3D;
+            thirdDialogue_Companion.Controls.Add(wall8);
+            thirdDialogue_Companion.Location = new Point(-12, 89);
+            thirdDialogue_Companion.Name = "thirdDialogue_Companion";
+            thirdDialogue_Companion.Size = new Size(898, 133);
+            thirdDialogue_Companion.TabIndex = 17;
+            thirdDialogue_Companion.Visible = false;
+            // 
+            // Message2
+            // 
+            Message2.AutoSize = true;
+            Message2.BackColor = Color.Transparent;
+            Message2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            Message2.ForeColor = Color.White;
+            Message2.Location = new Point(47, 68);
+            Message2.Name = "Message2";
+            Message2.Size = new Size(88, 28);
+            Message2.TabIndex = 1;
+            Message2.Text = "Message";
+            // 
+            // Name2
+            // 
+            Name2.AutoSize = true;
+            Name2.BackColor = Color.Transparent;
+            Name2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            Name2.ForeColor = Color.White;
+            Name2.Location = new Point(47, 37);
+            Name2.Name = "Name2";
+            Name2.Size = new Size(64, 28);
+            Name2.TabIndex = 0;
+            Name2.Text = "Name";
+            // 
+            // thirdDialogue
+            // 
+            thirdDialogue.BackColor = Color.Black;
+            thirdDialogue.BorderStyle = BorderStyle.Fixed3D;
+            thirdDialogue.Controls.Add(Message3);
+            thirdDialogue.Controls.Add(Name3);
+            thirdDialogue.Location = new Point(-32, -16);
+            thirdDialogue.Name = "thirdDialogue";
+            thirdDialogue.Size = new Size(914, 173);
+            thirdDialogue.TabIndex = 16;
+            thirdDialogue.Visible = false;
+            // 
+            // Message3
+            // 
+            Message3.AutoSize = true;
+            Message3.BackColor = Color.Transparent;
+            Message3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            Message3.ForeColor = Color.White;
+            Message3.Location = new Point(73, 96);
+            Message3.Name = "Message3";
+            Message3.Size = new Size(88, 28);
+            Message3.TabIndex = 1;
+            Message3.Text = "Message";
+            // 
+            // Name3
+            // 
+            Name3.AutoSize = true;
+            Name3.BackColor = Color.Transparent;
+            Name3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            Name3.ForeColor = Color.White;
+            Name3.Location = new Point(73, 65);
+            Name3.Name = "Name3";
+            Name3.Size = new Size(64, 28);
+            Name3.TabIndex = 0;
+            Name3.Text = "Name";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(856, 518);
+            Controls.Add(thirdDialogue);
+            Controls.Add(secondDialogue);
+            Controls.Add(firstDialogue_Companion);
+            Controls.Add(firstDialogue);
+            Controls.Add(sibling);
+            Controls.Add(mother);
+            Controls.Add(father);
+            Controls.Add(protagonist);
+            Controls.Add(window);
+            Controls.Add(wall7);
+            Controls.Add(wall2);
+            Controls.Add(wall1);
+            Controls.Add(wall4);
+            Controls.Add(wall6);
+            Controls.Add(wall5);
+            Controls.Add(wall3);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)wall3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)window).EndInit();
+            ((System.ComponentModel.ISupportInitialize)protagonist).EndInit();
+            ((System.ComponentModel.ISupportInitialize)father).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mother).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sibling).EndInit();
+            firstDialogue.ResumeLayout(false);
+            firstDialogue.PerformLayout();
+            secondDialogue.ResumeLayout(false);
+            secondDialogue.PerformLayout();
+            thirdDialogue_Companion.ResumeLayout(false);
+            thirdDialogue.ResumeLayout(false);
+            thirdDialogue.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox wall3;
+        private PictureBox wall5;
+        private PictureBox wall6;
+        private PictureBox wall4;
+        private PictureBox wall1;
+        private PictureBox wall2;
+        private PictureBox wall8;
+        private PictureBox wall7;
+        private PictureBox window;
+        private PictureBox protagonist;
+        private PictureBox father;
+        private PictureBox mother;
+        private PictureBox sibling;
+        private Panel firstDialogue;
+        private Panel firstDialogue_Companion;
+        private Label Message1;
+        private Label Name1;
+        private Panel secondDialogue;
+        private Panel thirdDialogue_Companion;
+        private Label Message2;
+        private Label Name2;
+        private Panel thirdDialogue;
+        private Label Message3;
+        private Label Name3;
     }
 }
--- Post-Mission 1/Form1.cs
+++ Post-Mission 1/Form1.cs
@@ -2,9 +2,153 @@ namespace Post_Mission_1
 {
     public partial class Form1 : Form
     {
+        private int dialogueCounter = 0;
+        private int msgIndex = 0;
+        private string MsgText = "Hey guys! I'm back from handing out all the flyers!";
+        private System.Windows.Forms.Timer animationTimer;
+        private bool isLivingRoomShown = false; // Add this flag
+
         public Form1()
         {
             InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.MouseClick += new MouseEventHandler(MouseClick_Part1);
+        }
+
+        /* panels have the corresponding names:
+        firstDialogue              Name1
+        firstDialogue_Companion    Message1
+
+        secondDialogue             Name2
+        secondDialogue_Companion   Message2
+
+        thirdDialogue              Name3
+        thirdDialogue_Companion    Message3
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Name1.Text = "Protagonist";
+            Message1.Text = string.Empty;
+
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+        }
+
+        private void AnimateText(object sender, EventArgs e)
+        {
+            //start of 2nd dialogue
+            if (dialogueCounter == 1)
+            {
+                secondDialogue.Visible = true;
+
+                firstDialogue.Visible = false;
+                firstDialogue_Companion.Visible = false;
+
+                Name2.Text = "Father";
+                MsgText = "Good job, anak. But this is just the beginning. We need more support.";
+            }
+            //third dialogue
+            else if (dialogueCounter == 2)
+            {
+                secondDialogue.Visible = false;
+
+                thirdDialogue.Visible = true;
+                thirdDialogue_Companion.Visible = true;
+
+                Name3.Text = "Protagonist";
+                MsgText = "I agree dad! I'm going to rest alrady. Good bye!";
+            }
+            else if (dialogueCounter == 3 && !isLivingRoomShown)
+            {
+                isLivingRoomShown = true;
+                PM1_LivingRoom pM1_LivingRoom = new PM1_LivingRoom();
+                pM1_LivingRoom.Show();
+                this.Hide();
+            }
+            if (msgIndex < MsgText.Length)
+            {
+                if (dialogueCounter == 0)
+                {
+                    Message1.Text += MsgText[msgIndex];
+                }
+                else if (dialogueCounter == 1)
+                {
+                    Message2.Text += MsgText[msgIndex];
+                }
+                else if (dialogueCounter == 2)
+                {
+                    Message3.Text += MsgText[msgIndex];
+                }
+                msgIndex++;
+            }
+            else
+            {
+                animationTimer.Stop();
+            }
+        }
+
+        // start of 2nd dialogue
+        private void MouseClick_Part1(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++; // dialogueCounter = 1
+            Message2.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part1; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part2; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 3rd dialogue
+        private void MouseClick_Part2(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++; // dialogueCounter = 2
+            Message3.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Add MouseClick_Part2 event handler after the father's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part2; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part3; // Add the MouseClick_Part2 event handler
+                }
+            });
+        }
+
+        // 3rd dialogue
+        private void MouseClick_Part3(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++; // dialogueCounter = 3
+            Message3.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
         }
     }
 }
