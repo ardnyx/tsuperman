499dbfc6c435b9ec91f355714fba8060cf06a6ad
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-27 01:47:24 +0800

    completed basic functionalities

---
+++ Post-Mission 1/PM1_LivingRoom.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Post_Mission_1
+{
+    partial class PM1_LivingRoom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            wall_1 = new PictureBox();
+            wall_8 = new PictureBox();
+            wall_2 = new PictureBox();
+            wall_3 = new PictureBox();
+            wall_4 = new PictureBox();
+            sibling = new PictureBox();
+            wall_5 = new PictureBox();
+            mother = new PictureBox();
+            father = new PictureBox();
+            wall_6 = new PictureBox();
+            wall_7 = new PictureBox();
+            window = new PictureBox();
+            protagonist = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)wall_1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)sibling).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)mother).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)father).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)wall_7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)window).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)protagonist).BeginInit();
+            SuspendLayout();
+            // 
+            // wall_1
+            // 
+            wall_1.BackColor = Color.Black;
+            wall_1.Location = new Point(-9, 445);
+            wall_1.Name = "wall_1";
+            wall_1.Size = new Size(883, 83);
+            wall_1.TabIndex = 17;
+            wall_1.TabStop = false;
+            // 
+            // wall_8
+            // 
+            wall_8.BackColor = Color.Black;
+            wall_8.Location = new Point(-9, 345);
+            wall_8.Name = "wall_8";
+            wall_8.Size = new Size(96, 109);
+            wall_8.TabIndex = 18;
+            wall_8.TabStop = false;
+            // 
+            // wall_2
+            // 
+            wall_2.BackColor = Color.Black;
+            wall_2.Location = new Point(775, 345);
+            wall_2.Name = "wall_2";
+            wall_2.Size = new Size(99, 142);
+            wall_2.TabIndex = 19;
+            wall_2.TabStop = false;
+            // 
+            // wall_3
+            // 
+            wall_3.BackColor = Color.Black;
+            wall_3.Location = new Point(775, -21);
+            wall_3.Name = "wall_3";
+            wall_3.Size = new Size(111, 252);
+            wall_3.TabIndex = 20;
+            wall_3.TabStop = false;
+            // 
+            // wall_4
+            // 
+            wall_4.BackColor = Color.Black;
+            wall_4.Location = new Point(635, -73);
+            wall_4.Name = "wall_4";
+            wall_4.Size = new Size(214, 144);
+            wall_4.TabIndex = 21;
+            wall_4.TabStop = false;
+            // 
+            // sibling
+            // 
+            sibling.BackgroundImage = Properties.Resources.child_2;
+            sibling.BackgroundImageLayout = ImageLayout.Stretch;
+            sibling.Location = new Point(690, 65);
+            sibling.Name = "sibling";
+            sibling.Size = new Size(64, 127);
+            sibling.TabIndex = 22;
+            sibling.TabStop = false;
+            // 
+            // wall_5
+            // 
+            wall_5.BackColor = Color.Black;
+            wall_5.Location = new Point(316, -73);
+            wall_5.Name = "wall_5";
+            wall_5.Size = new Size(185, 144);
+            wall_5.TabIndex = 23;
+            wall_5.TabStop = false;
+            // 
+            // mother
+            // 
+            mother.BackgroundImage = Properties.Resources.mother;
+            mother.BackgroundImageLayout = ImageLayout.Stretch;
+            mother.Location = new Point(233, 65);
+            mother.Name = "mother";
+            mother.Size = new Size(86, 146);
+            mother.TabIndex = 26;
+            mother.TabStop = false;
+            // 
+            // father
+            // 
+            father.BackgroundImage = Properties.Resources.father;
+            father.BackgroundImageLayout = ImageLayout.Stretch;
+            father.Location = new Point(128, 46);
+            father.Name = "father";
+            father.Size = new Size(90, 165);
+            father.TabIndex = 25;
+            father.TabStop = false;
+            // 
+            // wall_6
+            // 
+            wall_6.BackColor = Color.Black;
+            wall_6.Location = new Point(-28, -44);
+            wall_6.Name = "wall_6";
+            wall_6.Size = new Size(219, 115);
+            wall_6.TabIndex = 24;
+            wall_6.TabStop = false;
+            // 
+            // wall_7
+            // 
+            wall_7.BackColor = Color.Black;
+            wall_7.Location = new Point(-9, 29);
+            wall_7.Name = "wall_7";
+            wall_7.Size = new Size(96, 202);
+            wall_7.TabIndex = 28;
+            wall_7.TabStop = false;
+            // 
+            // window
+            // 
+            window.BackColor = Color.Brown;
+            window.Location = new Point(155, 423);
+            window.Name = "window";
+            window.Size = new Size(184, 31);
+            window.TabIndex = 29;
+            window.TabStop = false;
+            // 
+            // protagonist
+            // 
+            protagonist.BackgroundImage = Properties.Resources.child_1;
+            protagonist.BackgroundImageLayout = ImageLayout.Stretch;
+            protagonist.Location = new Point(205, 281);
+            protagonist.Name = "protagonist";
+            protagonist.Size = new Size(73, 136);
+            protagonist.TabIndex = 30;
+            protagonist.TabStop = false;
+            // 
+            // PM1_LivingRoom
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(856, 518);
+            Controls.Add(protagonist);
+            Controls.Add(window);
+            Controls.Add(wall_7);
+            Controls.Add(mother);
+            Controls.Add(father);
+            Controls.Add(wall_6);
+            Controls.Add(wall_5);
+            Controls.Add(sibling);
+            Controls.Add(wall_4);
+            Controls.Add(wall_3);
+            Controls.Add(wall_2);
+            Controls.Add(wall_8);
+            Controls.Add(wall_1);
+            Name = "PM1_LivingRoom";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PM1_LivingRoom";
+            Load += PM1_LivingRoom_Load;
+            KeyDown += keyIsDown;
+            KeyUp += keyIsUp;
+            ((System.ComponentModel.ISupportInitialize)wall_1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)sibling).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)mother).EndInit();
+            ((System.ComponentModel.ISupportInitialize)father).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)wall_7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)window).EndInit();
+            ((System.ComponentModel.ISupportInitialize)protagonist).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox wall_1;
+        private PictureBox wall_8;
+        private PictureBox wall_2;
+        private PictureBox wall_3;
+        private PictureBox wall_4;
+        private PictureBox sibling;
+        private PictureBox wall_5;
+        private PictureBox mother;
+        private PictureBox father;
+        private PictureBox wall_6;
+        private PictureBox wall_7;
+        private PictureBox window;
+        private PictureBox protagonist;
+    }
+}
---
+++ Post-Mission 1/PM1_LivingRoom.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Post_Mission_1
+{
+    public partial class PM1_LivingRoom : Form
+    {
+        bool goUp, goDown, goLeft, goRight;
+        int speed = 5;
+        List<PictureBox> walls = new List<PictureBox>();
+        System.Windows.Forms.Timer moveTimer;
+        public PM1_LivingRoom()
+        {
+            InitializeComponent();
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            MoveProtagonist();
+        }
+
+        private void MoveProtagonist()
+        {
+            if (protagonist.Left <= 0)
+            {
+                // show new form
+                return; // Exit the method to avoid further processing
+            }
+            if (goUp && protagonist.Top > 0 && !IsCollision(new Point(protagonist.Left, protagonist.Top - speed)))
+            {
+                protagonist.Top -= speed;
+            }
+            if (goDown && protagonist.Bottom < this.ClientSize.Height && !IsCollision(new Point(protagonist.Left, protagonist.Top + speed)))
+            {
+                protagonist.Top += speed;
+            }
+            if (goLeft && protagonist.Left > 0 && !IsCollision(new Point(protagonist.Left - speed, protagonist.Top)))
+            {
+                protagonist.Left -= speed;
+            }
+            if (goRight && protagonist.Right < this.ClientSize.Width && !IsCollision(new Point(protagonist.Left + speed, protagonist.Top)))
+            {
+                protagonist.Left += speed;
+            }
+        }
+
+        private bool IsCollision(Point newLocation)
+        {
+            // Define the lower portion of the protagonist for collision detection
+            int lowerBodyHeight = protagonist.Height / 3;
+            Rectangle lowerBodyBounds = new Rectangle(
+                newLocation.X,
+                newLocation.Y + (protagonist.Height - lowerBodyHeight),
+                protagonist.Width,
+                lowerBodyHeight
+            );
+
+            foreach (var wall in walls)
+            {
+                if (lowerBodyBounds.IntersectsWith(wall.Bounds))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void PM1_LivingRoom_Load(object sender, EventArgs e)
+        {
+            // Set up the Timer
+            moveTimer = new System.Windows.Forms.Timer();
+            moveTimer.Interval = 20; // Adjust as needed
+            moveTimer.Tick += new EventHandler(MoveTimer_Tick);
+            moveTimer.Start();
+
+            // Register key events
+            this.KeyDown += new KeyEventHandler(keyIsDown);
+            this.KeyUp += new KeyEventHandler(keyIsUp);
+
+            // Add your wall PictureBoxes to the list
+            // For example:
+            walls.Add(wall_1);
+            walls.Add(wall_2);
+            walls.Add(wall_3);
+            walls.Add(wall_4);
+            walls.Add(wall_5);
+            walls.Add(wall_6);
+            walls.Add(wall_7);
+            walls.Add(wall_8);
+
+            walls.Add(father);
+            walls.Add(mother);
+            walls.Add(sibling);
+            walls.Add(window);
+        }
+    }
+}
