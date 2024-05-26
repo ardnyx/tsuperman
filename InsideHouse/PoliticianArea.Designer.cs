13f370e8aab7f4cdea12b5d49473dd0fa297e12f
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-26 21:01:48 +0800

    added movement control in the politician area form

--- InsideHouse/PoliticianArea.Designer.cs
+++ InsideHouse/PoliticianArea.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "PoliticianArea";
+            protagonist = new PictureBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)protagonist).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // protagonist
+            // 
+            protagonist.BackColor = Color.Transparent;
+            protagonist.Image = Properties.Resources._51;
+            protagonist.Location = new Point(12, 346);
+            protagonist.Name = "protagonist";
+            protagonist.Size = new Size(74, 130);
+            protagonist.SizeMode = PictureBoxSizeMode.StretchImage;
+            protagonist.TabIndex = 12;
+            protagonist.TabStop = false;
+            protagonist.Tag = "child";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.local_politician;
+            pictureBox1.Location = new Point(682, 325);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(96, 151);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 13;
+            pictureBox1.TabStop = false;
+            pictureBox1.Tag = "child";
+            // 
+            // PoliticianArea
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.city_background;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(856, 518);
+            Controls.Add(pictureBox1);
+            Controls.Add(protagonist);
+            DoubleBuffered = true;
+            Name = "PoliticianArea";
+            Text = "PoliticianArea";
+            KeyDown += keyIsDown;
+            KeyUp += keyIsUp;
+            ((System.ComponentModel.ISupportInitialize)protagonist).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox protagonist;
+        private PictureBox pictureBox1;
     }
 }
--- InsideHouse/PoliticianArea.cs
+++ InsideHouse/PoliticianArea.cs
@@ -12,9 +12,72 @@ namespace InsideHouse
 {
     public partial class PoliticianArea : Form
     {
+        bool goLeft, goRight;
+        int speed = 5; // Speed of the player
+        System.Windows.Forms.Timer moveTimer; // dont know what is this for basta sabi ni chatgpt
+
         public PoliticianArea()
         {
             InitializeComponent();
+            // Set up the Timer
+            moveTimer = new System.Windows.Forms.Timer();
+            moveTimer.Interval = 20; // Adjust as needed
+            moveTimer.Tick += new EventHandler(MoveTimer_Tick);
+            moveTimer.Start();
+
+            // Register key events
+            this.KeyDown += new KeyEventHandler(keyIsDown);
+            this.KeyUp += new KeyEventHandler(keyIsUp);
+        }
+
+        // kailangan mo to isetup sa Event Handlers sa form, click mo ung form, tas sa gilid ng properties may parang lightning bolt don, tas hanapin mo yung keyDown, pinangalanan kong keyIsDown
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+
+        }
+
+        // did the same thing here
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+
+        private void MoveTimer_Tick(object sender, EventArgs e)
+        {
+            MoveProtagonist();
+        }
+
+        // eto, para to sa movement ng player
+        private void MoveProtagonist()
+        {
+            if (goLeft && protagonist.Left > 0)
+            {
+                protagonist.Left -= speed;
+            }
+            if (goRight && protagonist.Right < this.ClientSize.Width)
+            {
+                protagonist.Left += speed;
+            }
+
+            if (protagonist.Left <= 0)
+            {
+                TalkToPoliticianArea();
+            }
         }
     }
 }
