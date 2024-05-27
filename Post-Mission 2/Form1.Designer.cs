e6f8bae98215cb41d20c93e85d05c7a94cdddb34
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-27 15:36:36 +0800

    added all functionalities for post-mission 2

--- Post-Mission 2/Form1.Designer.cs
+++ Post-Mission 2/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            playerMsg = new Label();
+            playerName = new Label();
+            panel2 = new Panel();
+            backgroundChanger = new System.Windows.Forms.Timer(components);
+            beforeTextAppears = new System.Windows.Forms.Timer(components);
+            endLabel = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Black;
+            panel1.BorderStyle = BorderStyle.Fixed3D;
+            panel1.Controls.Add(playerMsg);
+            panel1.Controls.Add(playerName);
+            panel1.Location = new Point(-17, 374);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(888, 178);
+            panel1.TabIndex = 0;
+            // 
+            // playerMsg
+            // 
+            playerMsg.AutoSize = true;
+            playerMsg.BackColor = Color.Transparent;
+            playerMsg.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            playerMsg.ForeColor = Color.White;
+            playerMsg.Location = new Point(64, 62);
+            playerMsg.Name = "playerMsg";
+            playerMsg.Size = new Size(50, 28);
+            playerMsg.TabIndex = 1;
+            playerMsg.Text = "Msg";
+            // 
+            // playerName
+            // 
+            playerName.AutoSize = true;
+            playerName.BackColor = Color.Transparent;
+            playerName.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            playerName.ForeColor = Color.White;
+            playerName.Location = new Point(64, 34);
+            playerName.Name = "playerName";
+            playerName.Size = new Size(127, 28);
+            playerName.TabIndex = 0;
+            playerName.Text = "News Anchor";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Black;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Location = new Point(-17, -15);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(888, 146);
+            panel2.TabIndex = 1;
+            // 
+            // backgroundChanger
+            // 
+            backgroundChanger.Enabled = true;
+            backgroundChanger.Interval = 500;
+            backgroundChanger.Tick += backgroundChanger_Tick;
+            // 
+            // beforeTextAppears
+            // 
+            beforeTextAppears.Enabled = true;
+            beforeTextAppears.Interval = 1000;
+            beforeTextAppears.Tick += beforeTextAppears_Tick;
+            // 
+            // endLabel
+            // 
+            endLabel.AutoSize = true;
+            endLabel.BackColor = Color.Transparent;
+            endLabel.Font = new Font("Segoe UI", 23F, FontStyle.Regular, GraphicsUnit.Point);
+            endLabel.ForeColor = Color.White;
+            endLabel.Location = new Point(273, 236);
+            endLabel.Name = "endLabel";
+            endLabel.Size = new Size(0, 42);
+            endLabel.TabIndex = 2;
+            endLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.news1;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(856, 518);
+            Controls.Add(endLabel);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            DoubleBuffered = true;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel panel1;
+        private Label playerMsg;
+        private Label playerName;
+        private Panel panel2;
+        private System.Windows.Forms.Timer backgroundChanger;
+        private System.Windows.Forms.Timer beforeTextAppears;
+        private Label endLabel;
     }
 }
--- Post-Mission 2/Form1.cs
+++ Post-Mission 2/Form1.cs
@@ -2,9 +2,311 @@ namespace Post_Mission_2
 {
     public partial class Form1 : Form
     {
+        private int dialogueCounter = 0;
+        private int msgIndex = 0;
+        private string playerMsgText = "In recent news, the government is doubling down on the jeepney modernization program despite public protests.";
+        private System.Windows.Forms.Timer animationTimer;
+        private int caseSwitch = 0;
         public Form1()
         {
             InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.MouseClick += new MouseEventHandler(MouseClick_Part1);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            playerName.Text = "News Anchor";
+            playerMsg.Text = string.Empty;
+
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+        }
+
+        private void AnimateText(object sender, EventArgs e)
+        {
+            // 2nd dialogue
+            if (dialogueCounter == 1)
+            {
+                playerMsgText = "This program is crucial for our environment and public safety. We cannot delay its implementation.";
+                playerName.Text = "Government Official";
+            }
+            // all black transition
+            else if (dialogueCounter == 2)
+            {
+                backgroundChanger.Enabled = false;
+                this.BackgroundImage = null;
+                this.BackColor = Color.Black;
+                playerMsgText = "";
+                playerName.Text = "";
+            }
+            // wide view of the government talking
+            else if (dialogueCounter == 3)
+            {
+                this.BackgroundImage = Properties.Resources.government_talking;
+                playerMsgText = "";
+                playerName.Text = "";
+            }
+            // left convo government
+            else if (dialogueCounter == 4)
+            {
+                this.BackgroundImage = Properties.Resources.politics_left;
+                playerMsgText = "These new jeepneys are a lucrative business opportunity. We need to ensure this program goes through.";
+                playerName.Text = "Government Official";
+            }
+            // right convo government
+            else if (dialogueCounter == 5)
+            {
+                this.BackgroundImage = Properties.Resources.politics_right;
+                playerMsgText = "Don't worry, we have the support of key officials.";
+                playerName.Text = "Government Official";
+            }
+            // all black
+            else if (dialogueCounter == 6)
+            {
+                this.BackgroundImage = null;
+                this.BackColor = Color.Black;
+                playerMsgText = "";
+                playerName.Text = "";
+            }
+            // all black
+            else if (dialogueCounter == 7)
+            {
+                this.BackgroundImage = null;
+                this.BackColor = Color.Black;
+                playerMsgText = "To Be Continued";
+                playerName.Text = "";
+            }
+
+            if (msgIndex < playerMsgText.Length)
+            {
+                if (dialogueCounter == 7)
+                {
+                    endLabel.Visible = true;
+                    endLabel.Text += playerMsgText[msgIndex];
+                }
+                else playerMsg.Text += playerMsgText[msgIndex];
+                msgIndex++;
+            }
+            else
+            {
+                animationTimer.Stop();
+            }
+        }
+
+        // 2nd part of dialogue
+        private void MouseClick_Part1(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the protagonist's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part1; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part2; // Remove the MouseClick_Part1 event handler
+                }
+            });
+        }
+
+        // 3rd dialogue (only transitional, no text, black background only)
+        private void MouseClick_Part2(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the protagonist's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part2; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part3; // Remove the MouseClick_Part1 event handler
+                }
+            });
+        }
+
+        // 4th dialogue (wide view of the government officials)
+        private void MouseClick_Part3(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the protagonist's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part3; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part4; // Remove the MouseClick_Part1 event handler
+                }
+            });
+        }
+
+        // 5th dialogue (left convo)
+        private void MouseClick_Part4(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the protagonist's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part4; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part5; // Remove the MouseClick_Part1 event handler
+                }
+            });
+        }
+        // 6th dialogue (left convo)
+        private void MouseClick_Part5(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the protagonist's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part5; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part6; // Remove the MouseClick_Part1 event handler
+                }
+            });
+        }
+
+        // 7th dialogue (all black)
+        private void MouseClick_Part6(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+
+            // Ensure text wraps when dialogue exceeds form size
+            playerMsg.AutoSize = false;
+            playerMsg.Width = this.ClientSize.Width - playerMsg.Location.X * 2; // Adjust X position for padding
+            playerMsg.Height = this.ClientSize.Height - playerMsg.Location.Y * 2; // Adjust Y position for padding
+
+            // Add MouseClick_Part2 event handler after the protagonist's dialogue animation is complete
+            animationTimer.Tick += new EventHandler((s, args) =>
+            {
+                if (!animationTimer.Enabled)
+                {
+                    this.MouseClick -= MouseClick_Part6; // Remove the MouseClick_Part1 event handler
+                    this.MouseClick += MouseClick_Part7; // Remove the MouseClick_Part1 event handler
+                }
+            });
+        }
+        // 8th dialogue (to be continued)
+        private void MouseClick_Part7(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerMsg.Text = string.Empty;
+
+            // Start animation for the protagonist's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+        }
+
+        private void backgroundChanger_Tick(object sender, EventArgs e)
+        {
+            caseSwitch++;
+            switch (caseSwitch)
+            {
+                case 1:
+                    this.BackgroundImage = Properties.Resources.news1;
+                    break;
+                case 2:
+                    this.BackgroundImage = Properties.Resources.news2;
+                    break;
+            }
+
+            if (caseSwitch == 2) caseSwitch = 0;
+        }
+
+        private void beforeTextAppears_Tick(object sender, EventArgs e)
+        {
+            // none
         }
     }
 }
