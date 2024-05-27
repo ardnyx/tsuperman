edb616bc35d36294495466f1a357ac451ad54802
Reiyane <86859621+ardnyx@users.noreply.github.com>
2024-05-27 12:40:49 +0800

    completed the next day transition form

---
+++ Post-Mission 1/PM1_NextDayTransition.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Post_Mission_1
+{
+    partial class PM1_NextDayTransition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            playerName = new Label();
+            SuspendLayout();
+            // 
+            // playerName
+            // 
+            playerName.AutoSize = true;
+            playerName.BackColor = Color.Black;
+            playerName.Font = new Font("Segoe UI", 25F, FontStyle.Regular, GraphicsUnit.Point);
+            playerName.ForeColor = Color.White;
+            playerName.Location = new Point(339, 236);
+            playerName.Name = "playerName";
+            playerName.Size = new Size(178, 46);
+            playerName.TabIndex = 1;
+            playerName.Text = "Next Day...";
+            // 
+            // PM1_NextDayTransition
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(856, 518);
+            Controls.Add(playerName);
+            Name = "PM1_NextDayTransition";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PM1_NextDayTransition";
+            Load += PM1_NextDayTransition_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label playerName;
+    }
+}
---
+++ Post-Mission 1/PM1_NextDayTransition.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Post_Mission_1
+{
+    public partial class PM1_NextDayTransition : Form
+    {
+        bool nextFormOpen = false;
+        private int dialogueCounter = 0;
+        private int msgIndex = 0;
+        private string playerMsgText = "Next Day...";
+        private System.Windows.Forms.Timer animationTimer;
+        public PM1_NextDayTransition()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(PM1_NextDayTransition_Load);
+            this.MouseClick += new MouseEventHandler(OneClick);
+        }
+
+        private void PM1_NextDayTransition_Load(object sender, EventArgs e)
+        {
+            playerName.Text = string.Empty;
+
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+        }
+
+        private void AnimateText(object sender, EventArgs e)
+        {
+            if (dialogueCounter == 1 && !nextFormOpen)
+            {
+                nextFormOpen = true;
+                PM1_EnteringTheHouse pM1_EnteringTheHouse = new PM1_EnteringTheHouse();
+                pM1_EnteringTheHouse.Show();
+                this.Hide();
+                this.Close();
+                dialogueCounter = -1;
+            }
+
+            if (msgIndex < playerMsgText.Length)
+            {
+                playerName.Text += playerMsgText[msgIndex];
+                msgIndex++;
+            }
+            else
+            {
+                animationTimer.Stop();
+            }
+        }
+
+        private void OneClick(object sender, MouseEventArgs e)
+        {
+            dialogueCounter++;
+            playerName.Text = string.Empty;
+
+            // Start animation for the father's dialogue
+            msgIndex = 0;
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = 100; // Adjust the speed of the text animation
+            animationTimer.Tick += new EventHandler(AnimateText);
+            animationTimer.Start();
+        }
+    }
+}
